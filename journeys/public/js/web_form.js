929bcd092c451677881142c5307b199b69a2afb7
srijan <srijan@onehash.ai>
2021-09-10 17:38:42 +0530

    minor ui fix in web form

--- journeys/public/js/web_form.js
+++ journeys/public/js/web_form.js
@@ -1,5 +1,18 @@
 frappe.ui.form.on('Web Form', {
 
+    onload: function(frm) {
+		if (frm.doc.is_embeddable == 1) {
+            let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea></div></div>`
+            frm.set_df_property('embed_url', 'options', msg);
+        }
+	},
+	refresh: function(frm) {
+        if (frm.doc.is_embeddable == 1) {
+            let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea></div></div>`
+            frm.set_df_property('embed_url', 'options', msg);
+        }
+    },
+
 	is_embeddable: function (frm) {
 		frm.events.disable_enable_fields(frm);
 	},
