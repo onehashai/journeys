dfcffe63cdd8d1b7c23f14759487f43e6e898738
srijan <srijan@onehash.ai>
2021-09-13 12:06:19 +0530

    minor ui imrovement in web forms

--- journeys/public/js/web_form.js
+++ journeys/public/js/web_form.js
@@ -2,12 +2,30 @@ frappe.ui.form.on('Web Form', {
 
     onload: function(frm) {
 		if (frm.doc.is_embeddable == 1) {
-            let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea></div></div>`
+            frm.set_df_property('client_script', 'hidden', true);
+            frm.set_df_property('client_script_section', 'hidden', true);
+            frm.set_df_property('sidebar_items', 'hidden', true);
+            frm.set_df_property('sidebar_settings', 'hidden', true);
+            frm.doc.show_sidebar=0
+    		refresh_field("show_sidebar")
+            frm.doc.login_required=0
+    		refresh_field("login_required")
+            frm.set_df_property('login_required', 'hidden', true);
+            let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea><br><br> You can also add a paramter "&referer=yoursite" in the src url to track the referer of this form.</div></div>`
             frm.set_df_property('embed_url', 'options', msg);
         }
 	},
 	refresh: function(frm) {
         if (frm.doc.is_embeddable == 1) {
+            frm.set_df_property('client_script', 'hidden', true);
+            frm.set_df_property('client_script_section', 'hidden', true);
+            frm.set_df_property('sidebar_items', 'hidden', true);
+            frm.set_df_property('sidebar_settings', 'hidden', true);
+            frm.doc.show_sidebar=0
+    		refresh_field("show_sidebar")
+            frm.doc.login_required=0
+    		refresh_field("login_required")
+            frm.set_df_property('login_required', 'hidden', true);
             let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea><br><br> You can also add a paramter "&referer=yoursite" in the src url to track the referer of this form.</div></div>`
             frm.set_df_property('embed_url', 'options', msg);
         }
@@ -29,7 +47,7 @@ frappe.ui.form.on('Web Form', {
             frm.doc.login_required=0
     		refresh_field("login_required")
             frm.set_df_property('login_required', 'hidden', true);
-            let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea></div></div>`
+            let msg = `<div class="card mb-3 h-100"><div class="card-body"> <b>Embed URL</b> <br><br><textarea rows="4" cols="50"><iframe frameborder="0" style="height:500px;width:99%;border:none;" src='https://${frappe.boot.sitename}/api/method/journeys.users.forms?new=1&path=${frm.doc.name}'></iframe></textarea><br><br> You can also add a paramter "&referer=yoursite" in the src url to track the referer of this form.</div></div>`
             frm.set_df_property('embed_url', 'options', msg);
             frm.dirty()
         }
