f85ae41857861961842bb3491b0c997efce5900c
Narendra Yadav <narendra@onehash.ai>
2022-09-26 20:04:49 +0530

    WhatsApp Web Messages and OneCRM update

---
+++ journeys/journeys/doctype/whatsapp_web_message_template/whatsapp_web_message_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, OneHash Inc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WhatsApp Web Message Template', {
+	// refresh: function(frm) {
+
+	// }
+});
