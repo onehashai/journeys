53bf37c5a3b0cd0fad0d12cc58900e505b9ee603
Narendra Yadav <narendra@onehash.ai>
2022-12-23 10:43:22 +0530

    India mart version 2 api updated

--- journeys/journeys/doctype/indiamart_settings/indiamart_settings.py
+++ journeys/journeys/doctype/indiamart_settings/indiamart_settings.py
@@ -21,18 +21,47 @@ class IndiaMartSettings(Document):
 			pass
 	
 	def validate_access_credentials(self):
-		if not (self.crm_key and self.registered_mobile):
+	#	if not (self.crm_key and self.registered_mobile):
+		if not (self.crm_key):
 			frappe.msgprint(_("Missing value for CRM Key or Registered Mobile"), raise_exception=frappe.ValidationError)
 
 	def validate_request(self):
-		url = "https://mapi.indiamart.com/wservce/enquiry/listing/GLUSR_MOBILE/{0}/GLUSR_MOBILE_KEY/{1}/".format(self.registered_mobile,self.crm_key)
+		try:
+			settings = frappe.get_doc('IndiaMart Settings')
+		except Exception as e:
+			print(e)
+			settings=None
+			pass
+		if(settings==None):
+			return
+
+		if(not settings.enable_indiamart):
+			return 
+		end_time = frappe.utils.now()
+		if(settings and settings.last_sync_time):
+			start_time = settings.last_sync_time
+		else:
+			import datetime
+			start_time = datetime.datetime.now()-datetime.timedelta(minutes=15)
+
+		# url = "https://mapi.indiamart.com/wservce/enquiry/listing/GLUSR_MOBILE/{0}/GLUSR_MOBILE_KEY/{1}/".format(self.registered_mobile,self.crm_key)
+		# url =   "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key={0}/start_time={1}/end_time={2}/".format(self.crm_key,'21-Dec-2022','21-Dec-2022')
+		# url = "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key={0}".format(self.crm_key)
+		url = "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key={0}&start_time={1}&end_time={2}".format(settings.crm_key,start_time,end_time)
+		# url = "http://localhost/deetee/indiamart.json?glusr_crm_key=mR20E71p4H7AQfep5nWN7liKoVrAnjVj&start_time=19-Dec-2022&end_time=21-DEC-2022"
+		
 		session = get_request_session()
+		# d = session.get(url, data={},auth='', headers=None)
+		# d.raise_for_status()
+		# response = d.json()	
+		
 		try:
 			d = session.get(url, data={},auth='', headers=None)
 			d.raise_for_status()
-			response = d.json()			
-			if(response!=[] and response[0]["Error_Message"]):
-				frappe.msgprint(_(response[0]["Error_Message"]), raise_exception=frappe.ValidationError)
+			response = d.json()		
+			# frappe.log_error(response)
+			if(response!=[] and response["RESPONSE"]):
+				frappe.msgprint(_(response["RESPONSE"]), raise_exception=frappe.ValidationError)
 		except Exception as e:
 			frappe.log_error(frappe.get_traceback())
 			#raise e
@@ -84,35 +113,65 @@ def sync_enquiry():
 		import datetime
 		start_time = datetime.datetime.now()-datetime.timedelta(minutes=15)
 
-	url = "https://mapi.indiamart.com/wservce/enquiry/listing/GLUSR_MOBILE/{0}/GLUSR_MOBILE_KEY/{1}/Start_Time/{2}/End_Time/{3}/".format(settings.registered_mobile,settings.crm_key,start_time,end_time)
+#	url = "https://mapi.indiamart.com/wservce/enquiry/listing/GLUSR_MOBILE/{0}/GLUSR_MOBILE_KEY/{1}/Start_Time/{2}/End_Time/{3}/".format(settings.registered_mobile,settings.crm_key,start_time,end_time)
+#	url = "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key=mR20E71p4H7AQfep5nWN7liKoVrAnjVj&start_time=01-Jan-2022&end_time=06-Jan-2022"
+#	url = "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key={1}/start_time={2}/end_time={3}".format(settings.registered_mobile,settings.crm_key,start_time,end_time)
+ 	
+	url = "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key={0}&start_time={1}&end_time={2}".format(settings.crm_key,start_time,end_time)
+	
+	# url = "https://mapi.indiamart.com/wservce/crm/crmListing/v2/?glusr_crm_key={0}&start_time=19-Dec-2022&end_time=21-DEC-2022".format(settings.crm_key,start_time,end_time)
+	
+#	frappe.log_error(url)
 	session = get_request_session()
 	try:
 		d = session.get(url, data={},auth='', headers=None)
 		d.raise_for_status()
-		response = d.json()		
-		if(len(response)>0 and "Error_Message" in response[0]):
-			if response[0]["Error_Message"]!="There are no leads in the given time duration.please try for a different duration.":
-				frappe.msgprint(_(response[0]["Error_Message"]), raise_exception=frappe.ValidationError)
+		response = d.json()	
+		# frappe.log_error(response['MESSAGE'],'tesponsev testing')
+		
+		
+		if(len(response)>0):
+			if(response["STATUS"]=="FAILURE"): 
+				if(response["MESSAGE"]!="There are no leads in the given time duration. please try for a different duration."): 
+					frappe.msgprint(_(response["MESSAGE"]), raise_exception=frappe.ValidationError)
+				elif response["MESSAGE"]!="It is advised to hit this API once in every 5 minutes,but it seems that you have crossed this limit. Please try again after 5 minutes.":
+					frappe.msgprint(_(response["MESSAGE"]), raise_exception=frappe.ValidationError)
 			else:
 				settings.last_sync_time = end_time
 				settings.save()
-		else:
-			for opportunity in response:
-				opp_name = frappe.db.get_value("Opportunity", {"indiamart_query_id": opportunity["QUERY_ID"]})
+				# else:
+				# frappe.log_error(response["RESPONSE"],"responsedsbf")
+				# if(len(response)>0 and response['STATUS']=='FAILURE'):
+				
+				# if(response['STATUS']=='FAILURE'):
+				# 	if response["MESSAGE"]!="There are no leads in the given time duration.please try for a different duration.":
+				# 		frappe.msgprint("Diffrenct duration")
+				# 		# frappe.msgprint(_(response["MESSAGE"]), raise_exception=frappe.ValidationError)
+				# 	if response["MESSAGE"]!="It is advised to hit this API once in every 5 minutes,but it seems that you have crossed this limit. Please try again after 5 minutes.":
+				# 		frappe.msgprint("5 minutes error")
+				# 	else:
+				# 		settings.last_sync_time = end_time
+				# 		settings.save()
+					# if (len(response)>0):
+					# frappe.log_error("ho Gya")
+					# frappe.log_error(response["RESPONSE"],"responsedsbf")
+			for opportunity in response["RESPONSE"]:
+				# frappe.log_error(opportunity)
+				opp_name = frappe.db.get_value("Opportunity", {"indiamart_query_id": opportunity["UNIQUE_QUERY_ID"]})
 				if opp_name:
 					continue 
 
 				new_opportunity =frappe.get_doc({
 					"doctype":"Opportunity",
-					"indiamart_query_id":opportunity["QUERY_ID"],
-					"indiamart_qtype":opportunity["QUERY_MODID"],
-					"contact_email":opportunity["SENDEREMAIL"],
-					"contact_mobile":opportunity["MOB"],
+					"indiamart_query_id":opportunity["UNIQUE_QUERY_ID"],
+					"indiamart_qtype":opportunity["QUERY_TYPE"],
+					"contact_email":opportunity["SENDER_EMAIL"],
+					"contact_mobile":opportunity["SENDER_MOBILE"],
 					"title":opportunity["SUBJECT"],
-					"customer_name":opportunity["SENDERNAME"],
-					"opportunity_date":opportunity["DATE_RE"],
-					"address_display":opportunity["ENQ_ADDRESS"],
-					"enq_message":opportunity["ENQ_MESSAGE"],
+					"customer_name":opportunity["SENDER_COMPANY"],
+					"opportunity_date":opportunity["QUERY_TIME"],
+					"address_display":opportunity["SENDER_ADDRESS"],
+					"enq_message":opportunity["QUERY_MESSAGE"],
 					"source":"IndiaMart"
 				})
 				result = make_new_lead_if_required(opportunity)				
@@ -144,27 +203,28 @@ def make_new_lead_if_required(opportunity):
 			ifnull(`tabDynamic Link`.link_name, '')<>''
 		and
 			`tabDynamic Link`.link_doctype='Customer'
-	""".format(opportunity["SENDEREMAIL"]), as_dict=True)
+	""".format(opportunity["SENDER_EMAIL"]), as_dict=True)
 	if customer and customer[0].customer:
 		party_name = customer[0].customer
 		opportunity_from = "Customer"
 		return opportunity_from,party_name
 
-	if opportunity["SENDEREMAIL"] is not None:
-		lead_name = frappe.db.get_value("Lead", {"email_id": opportunity["SENDEREMAIL"]})
+	if opportunity["SENDER_EMAIL"] is not None:
+		lead_name = frappe.db.get_value("Lead", {"email_id": opportunity["SENDER_EMAIL"]})
 	else:
 		lead_name=""
 
 	if not lead_name:
-		sender_name = opportunity["SENDERNAME"]
+		sender_name = opportunity["SENDER_NAME"]
 		lead = frappe.get_doc({
 			"doctype": "Lead",
-			"email_id": (opportunity["SENDEREMAIL"] if opportunity["SENDEREMAIL"] else ""),
+			"email_id": (opportunity["SENDER_EMAIL"] if opportunity["SENDER_EMAIL"] else ""),
 			"source":"IndiaMart",
 			"lead_name": sender_name or 'Unknown',
-			"mobile_no":opportunity["MOB"],
-			"company_name":opportunity["GLUSR_USR_COMPANYNAME"],
-			"organization_lead": 1 if opportunity["GLUSR_USR_COMPANYNAME"] else 0
+			"mobile_no":opportunity["SENDER_MOBILE"],
+			"company_name":opportunity["SENDER_COMPANY"],
+			# "organization_lead": 1 if opportunity["GLUSR_USR_COMPANYNAME"] else 0
+			"organization_lead": 1 if opportunity["SENDER_COMPANY"] else 0
 		})
 		#lead.flags.ignore_email_validation = True
 		lead.insert(ignore_permissions=True)
@@ -179,30 +239,30 @@ def make_new_lead_if_required(opportunity):
 def create_contact(enquiry,lead_name):
 	contact = frappe.get_doc({
 		"doctype":"Contact",
-		"first_name":enquiry["SENDERNAME"],
-		"email_id":enquiry["SENDEREMAIL"],
-		"mobile_no":enquiry["MOB"]
+		"first_name":enquiry["SENDER_NAME"],
+		"email_id":enquiry["SENDER_EMAIL"],
+		"mobile_no":enquiry["SENDER_MOBILE"]
 	})
 
 	email_ids_obj = {}
-	if(enquiry["SENDEREMAIL"]):
-		contact.append('email_ids',{"email_id":enquiry["SENDEREMAIL"],"is_primary":1})
+	if(enquiry["SENDER_EMAIL"]):
+		contact.append('email_ids',{"email_id":enquiry["SENDER_EMAIL"],"is_primary":1})
 
-	if(enquiry["EMAIL_ALT"]):
-		contact.append('email_ids',{"email_id":enquiry["EMAIL_ALT"],"is_primary":0})
+	if(enquiry["SENDER_EMAIL_ALT"]):
+		contact.append('email_ids',{"email_id":enquiry["SENDER_EMAIL_ALT"],"is_primary":0})
 
 	phone_nos_object={}
-	if(enquiry["MOB"]):
-		contact.append('phone_nos',{"phone":enquiry["MOB"],"is_primary_mobile":1})
+	if(enquiry["SENDER_MOBILE"]):
+		contact.append('phone_nos',{"phone":enquiry["SENDER_MOBILE"],"is_primary_mobile":1})
 	
-	if(enquiry["PHONE"]):
-		contact.append('phone_nos',{"phone":enquiry["PHONE"],"is_primary_phone":1})
+	if(enquiry["SENDER_PHONE"]):
+		contact.append('phone_nos',{"phone":enquiry["SENDER_PHONE"],"is_primary_phone":1})
 	
-	if(enquiry["MOBILE_ALT"]):
-		contact.append('phone_nos',{"phone":enquiry["MOBILE_ALT"],"is_primary_phone":0,"is_primary_mobile":0})
+	if(enquiry["SENDER_MOBILE_ALT"]):
+		contact.append('phone_nos',{"phone":enquiry["SENDER_MOBILE_ALT"],"is_primary_phone":0,"is_primary_mobile":0})
 	
-	if(enquiry["PHONE_ALT"]):
-		contact.append('phone_nos',{"phone":enquiry["PHONE_ALT"],"is_primary_phone":0,"is_primary_mobile":0})
+	if(enquiry["SENDER_PHONE_ALT"]):
+		contact.append('phone_nos',{"phone":enquiry["SENDER_PHONE_ALT"],"is_primary_phone":0,"is_primary_mobile":0})
 	
 	#contact.append('email_ids',email_ids_obj)
 	#contact.append('phone_nos',phone_nos_object)
@@ -214,10 +274,10 @@ def create_address(enquiry,lead_name):
 	address = frappe.get_doc({
 		"doctype":"Address",
 		"address_type":"Billing",
-		"address_line1":enquiry["ENQ_ADDRESS"],
-		"city":enquiry["ENQ_CITY"],
-		"state":enquiry["ENQ_STATE"],
-		"country": "India" if enquiry["COUNTRY_ISO"]=="IN" else ""
+		"address_line1":enquiry["SENDER_ADDRESS"],
+		"city":enquiry["SENDER_CITY"],
+		"state":enquiry["SENDER_STATE"],
+		"country": "India" if enquiry["SENDER_COUNTRY_ISO"]=="IN" else ""
 	})
 	address.append("links",{"link_doctype":"Lead","link_name":lead_name})
 	address.insert(ignore_mandatory=True,ignore_permissions=True)
